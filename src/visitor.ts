21b7551aa33369e803e16efa4d6cb4355f11b4bc
Matt Bishop <matt@thebishops.org>
2022-02-20 12:15:07 -0800

    Remove more unused code that has been replaced by WFF.

--- src/visitor.ts
+++ src/visitor.ts
@@ -1,4 +1,4 @@
-import { CstNode, ICstVisitor } from "@chevrotain/types"
+import {CstNode, ICstVisitor} from "@chevrotain/types"
 import {
   ArrayElement,
   ConditionalOperator,
@@ -12,10 +12,10 @@ import {
   PathPart,
   PathQuery,
   SimpleProperty,
-  SqlJsonPathArithmeticOperator,
   SqlJsonPathStatement,
   WFF
 } from "./json-path"
+
 
 export function newJsonPathVisitor(constr: { new(...args: any[]): ICstVisitor<any, any> }) {
   return new class JsonPathVisitor extends constr {
@@ -35,13 +35,6 @@ export function newJsonPathVisitor(constr: { new(...args: any[]): ICstVisitor<an
         obj.lhs = this.visit(ctx.lhs)
       }
 
-      if (ctx.rhs && ctx.operator) {
-        obj.operation = {
-          operator: this.visit(ctx.operator),
-          operand: this.visit(ctx.rhs)
-        }
-      }
-
       return obj
     }
 
@@ -49,12 +42,6 @@ export function newJsonPathVisitor(constr: { new(...args: any[]): ICstVisitor<an
       return ctx.strict
         ? "strict"
         : "lax"
-    }
-
-    operand(ctx: any): PathQuery | number {
-      return (ctx.path)
-        ? this.visit(ctx.path)
-        : ctx.number
     }
 
     arguments(ctx: any): string[] {
@@ -128,23 +115,6 @@ export function newJsonPathVisitor(constr: { new(...args: any[]): ICstVisitor<an
           return ConditionalOperator.NE
         default:
           throw Error("Unknown conditional operator: " + ctx.operator[0].image)
-      }
-    }
-
-    arithmeticOperator(ctx: any): SqlJsonPathArithmeticOperator {
-      switch (ctx.operator) {
-        case "+":
-          return SqlJsonPathArithmeticOperator.ADD
-        case "-":
-          return SqlJsonPathArithmeticOperator.SUB
-        case "*":
-          return SqlJsonPathArithmeticOperator.MUL
-        case "/":
-          return SqlJsonPathArithmeticOperator.DIV
-        case "%":
-          return SqlJsonPathArithmeticOperator.MOD
-        default:
-          throw new Error("Unknown operator type: " + ctx.operator)
       }
     }
 
