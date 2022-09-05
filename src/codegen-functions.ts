b0a0f87f9c84cfc59bb244a109dcc534f123daf1
Matt Bishop <matt@thebishops.org>
2022-09-05 14:42:52 -0700

    Change codegen to work with iterators instead of arrays. More SQL-like, easier to chain them together.

--- src/codegen-functions.ts
+++ src/codegen-functions.ts
@@ -1,115 +1,134 @@
+import {iterate} from "iterare"
+import {IteratorWithOperators} from "iterare/lib/iterate"
 import {DateTime, FixedOffsetZone} from "luxon"
 import {KeyValue} from "./json-path"
 
 
-function _toKV(primary: object, id: number): KeyValue[] {
-  return Object.entries(primary)
-    .map(([key, value]) => ({id, key, value}))
+function _toKV(primary: Record<string, any>, id: number): IteratorWithOperators<KeyValue> {
+  return iterate(Object.keys(primary))
+    .map((key) => ({id, key, value: primary[key]}))
+}
+
+function _type(primary: any): string {
+  return Array.isArray(primary)
+    ? "array"
+    : primary === null
+      ? "null"
+      : typeof primary
 }
 
 
 export const codegenFunctions = {
 
-  type(primary: any): string {
-    return Array.isArray(primary)
-      ? "array"
-      : primary === null
-        ? "null"
-        : typeof primary
+  type(seq: IteratorWithOperators<any>): IteratorWithOperators<string> {
+    return seq.map(_type)
   },
 
 
-  size(primary: any): number {
-    return Array.isArray(primary) ? primary.length : 1
+  size(seq: IteratorWithOperators<any>): IteratorWithOperators<number> {
+    return seq.map((primary) => Array.isArray(primary) ? primary.length : 1);
   },
 
 
-  double(primary: any): number {
-    const type = this.type(primary)
-    if (type !== "number" && type !== "string") {
-      throw new Error(`double() value must be a number or string, found ${JSON.stringify(primary)}.`)
-    }
-    const n = Number(primary)
-    if (Number.isNaN(n)) {
-      throw new Error(`double() param ${primary} is not a representation of a number.`)
-    }
-    return n
-  },
-
-
-  ceiling(primary: any): number {
-    if (typeof primary !== "number") {
-      throw new Error(`$ceiling() param must be a number, found ${JSON.stringify(primary)}.`)
-    }
-    return Math.ceil(primary)
-  },
-
-
-  floor(primary: any): number {
-    if (typeof primary !== "number") {
-      throw new Error(`floor() param must be a number, found ${JSON.stringify(primary)}.`)
-    }
-    return Math.floor(primary)
-  },
-
-
-  abs(primary: any): number {
-    if (typeof primary !== "number") {
-      throw new Error(`abs() param must be a number, found ${JSON.stringify(primary)}.`)
-    }
-    return Math.abs(primary)
-  },
-
-
-  keyvalue(primary: any, lax: boolean): KeyValue[] {
-    const type = this.type(primary)
-    if (lax) {
-      if (type !== "object" && type !== "array") {
-        throw new Error(`keyvalue() param must be an object or array (in lax mode), found ${JSON.stringify(primary)}.`)
+  double(seq: IteratorWithOperators<any>): IteratorWithOperators<number> {
+    return seq.map((primary) => {
+      const type = _type(primary)
+      if (type !== "number" && type !== "string") {
+        throw new Error(`double() value must be a number or string, found ${JSON.stringify(primary)}.`)
       }
-      if (Array.isArray(primary)) {
-        return primary.reduce((acc, row, id) => {
-          if (this.type(row) !== "object") {
-            throw new Error(`keyvalue() array must have object values, found ${JSON.stringify(row)}.`)
-          }
-          acc.push(..._toKV(row, id))
-          return acc
-        }, [])
+      const n = Number(primary)
+      if (Number.isNaN(n)) {
+        throw new Error(`double() param ${primary} is not a representation of a number.`)
       }
-    } else { // strict
-      if (type !== "object") {
-        throw new Error(`keyvalue() param must be an object but is an array (in strict mode), found ${JSON.stringify(primary)}.`)
-      }
-    }
-    return _toKV(primary, 0)
+      return n
+    })
   },
 
 
-  datetime(primary: any, template?: string): Date {
-    if (typeof primary !== "string") {
-      throw new Error(`datetime() param must be a string, found ${JSON.stringify(primary)}.`)
-    }
-    return template
-      ? DateTime.fromFormat(primary, template, {zone: FixedOffsetZone.utcInstance}).toJSDate()
-      : new Date(primary)
+  ceiling(seq: IteratorWithOperators<any>): IteratorWithOperators<number> {
+    return seq.map((primary) => {
+      if (typeof primary !== "number") {
+        throw new Error(`$ceiling() param must be a number, found ${JSON.stringify(primary)}.`)
+      }
+      return Math.ceil(primary)
+    })
   },
 
 
-  dotStar(primary: any, lax: boolean): any {
-    const type = this.type(primary)
-    if (!lax && type !== "object") {
-      throw new Error(`.* can only be applied to an object in strict mode, found ${JSON.stringify(primary)}.`)
-    }
-    let result
-    if (type === "object") {
-      result = Object.values(primary)
-    } else if (type === "array") {
-      result = primary
-        .filter((o: any) => this.type(o) === "object")
-        .flatMap(Object.values)
-    }
-    if (result && result.length) {
-      return result
-    }
+  floor(seq: IteratorWithOperators<any>): IteratorWithOperators<number> {
+    return seq.map(primary => {
+      if (typeof (primary) !== "number") {
+        throw new Error(`floor() param must be a number, found ${JSON.stringify(primary)}.`)
+      }
+      return Math.floor(primary)
+    })
+  },
+
+
+  abs(seq: IteratorWithOperators<any>): IteratorWithOperators<number> {
+    return seq.map((primary) => {
+      if (typeof primary !== "number") {
+        throw new Error(`abs() param must be a number, found ${JSON.stringify(primary)}.`)
+      }
+      return Math.abs(primary)
+    })
+  },
+
+
+  datetime(seq: IteratorWithOperators<any>, template?: string): IteratorWithOperators<Date> {
+    return seq.map((primary) => {
+      if (typeof primary !== "string") {
+        throw new Error(`datetime() param must be a string, found ${JSON.stringify(primary)}.`)
+      }
+      return template
+        ? DateTime.fromFormat(primary, template, {zone: FixedOffsetZone.utcInstance}).toJSDate()
+        : new Date(primary)
+    })
+  },
+
+
+  keyvalue(seq: IteratorWithOperators<any>, lax: boolean): IteratorWithOperators<KeyValue> {
+    return seq.map((primary) => {
+      const type = _type(primary)
+      if (lax) {
+        if (type !== "object" && type !== "array") {
+          throw new Error(`keyvalue() param must be an object or array (in lax mode), found ${JSON.stringify(primary)}.`)
+        }
+        if (Array.isArray(primary)) {
+          let id = 0
+          return iterate(primary)
+            .map((row) => {
+              if (_type(row) !== "object") {
+                throw new Error(`keyvalue() array must have object values, found ${JSON.stringify(row)}.`)
+              }
+              return _toKV(row, id++)
+            }).flatten()
+        }
+      } else { // strict
+        if (type !== "object") {
+          throw new Error(`keyvalue() param must be an object but is an array (in strict mode), found ${JSON.stringify(primary)}.`)
+        }
+      }
+      return _toKV(primary, 0)
+    }).flatten()
+  },
+
+
+  dotStar(seq: IteratorWithOperators<any>, lax: boolean): IteratorWithOperators<any> {
+    return seq.map((primary) => {
+      const type = _type(primary)
+      if (!lax && type !== "object") {
+        throw new Error(`.* can only be applied to an object in strict mode, found ${JSON.stringify(primary)}.`)
+      }
+      if (type === "object") {
+        return Object.values(primary)
+      } else if (type === "array") {
+        return iterate(primary)
+          .filter((o) => _type(o) === "object")
+          .map((o) => iterate(Object.values(o as object)))
+          .flatten()
+      }
+      return []
+    }).flatten()
   }
 }
--- test/codegen-test.ts
+++ test/codegen-test.ts
@@ -1,4 +1,6 @@
 import {expect} from "chai"
+import {iterate} from "iterare"
+import {IteratorWithOperators} from "iterare/lib/iterate"
 
 import {generateFunctionSource} from "../src"
 import {codegenFunctions} from "../src/codegen-functions"
@@ -6,7 +8,13 @@ import {codegenFunctions} from "../src/codegen-functions"
 
 function createFunction(body: string): Function {
   const fn = Function("$", "$$", body)
-  return fn.bind(codegenFunctions)
+  const boundFn = fn.bind(codegenFunctions)
+  return (input: any, args?: any) => {
+    const i = input instanceof IteratorWithOperators
+      ? input
+      : iterate([input])
+    return boundFn(i, args).toArray()
+  }
 }
 
 
@@ -36,7 +44,7 @@ describe("Codegen tests", () => {
       expect(actual.source).to.equal("return $")
       const fn = createFunction(actual.source)
       const value = fn("matt")
-      expect(value).to.equal("matt")
+      expect(value).to.deep.equal(["matt"])
     })
   })
 
@@ -46,19 +54,19 @@ describe("Codegen tests", () => {
       expect(actual.source).to.equal("return this.type($)")
       const fn = createFunction(actual.source)
       const nullType = fn(null)
-      expect(nullType).to.equal("null")
+      expect(nullType).to.deep.equal(["null"])
       const stringType = fn("matt")
-      expect(stringType).to.equal("string")
+      expect(stringType).to.deep.equal(["string"])
       const numberType = fn(77.6)
-      expect(numberType).to.equal("number")
+      expect(numberType).to.deep.equal(["number"])
       const booleanType = fn(true)
-      expect(booleanType).to.equal("boolean")
+      expect(booleanType).to.deep.equal(["boolean"])
       const objectType = fn({})
-      expect(objectType).to.equal("object")
+      expect(objectType).to.deep.equal(["object"])
       const arrayType = fn([])
-      expect(arrayType).to.equal("array")
+      expect(arrayType).to.deep.equal(["array"])
       const undefinedType = fn(undefined)
-      expect (undefinedType).to.equal("undefined")
+      expect (undefinedType).to.deep.equal(["undefined"])
     })
 
     it("size()", () => {
@@ -66,17 +74,17 @@ describe("Codegen tests", () => {
       expect(actual.source).to.equal("return this.size($)")
       const fn = createFunction(actual.source)
       const nullSize = fn(null)
-      expect(nullSize).to.equal(1)
+      expect(nullSize).to.deep.equal([1])
       const stringSize = fn("matt")
-      expect(stringSize).to.equal(1)
+      expect(stringSize).to.deep.equal([1])
       const numberSize = fn(77.6)
-      expect(numberSize).to.equal(1)
+      expect(numberSize).to.deep.equal([1])
       const booleanSize = fn(true)
-      expect(booleanSize).to.equal(1)
+      expect(booleanSize).to.deep.equal([1])
       const objectSize = fn({})
-      expect(objectSize).to.equal(1)
+      expect(objectSize).to.deep.equal([1])
       const arraySize = fn([1, 2, 3])
-      expect(arraySize).to.equal(3)
+      expect(arraySize).to.deep.equal([3])
     })
 
     it("double()", () => {
@@ -84,9 +92,9 @@ describe("Codegen tests", () => {
       expect(actual.source).to.equal("return this.double($)")
       const fn = createFunction(actual.source)
       const stringDouble = fn("45")
-      expect(stringDouble).to.equal(45)
+      expect(stringDouble).to.deep.equal([45])
       const numberDouble = fn(77.6)
-      expect(numberDouble).to.equal(77.6)
+      expect(numberDouble).to.deep.equal([77.6])
       expect(() => fn(null)).to.throw()
       expect(() => fn("bond")).to.throw()
       expect(() => fn(true)).to.throw()
@@ -99,7 +107,7 @@ describe("Codegen tests", () => {
       expect(actual.source).to.equal("return this.ceiling($)")
       const fn = createFunction(actual.source)
       const numberActual = fn(77.6)
-      expect(numberActual).to.equal(78)
+      expect(numberActual).to.deep.equal([78])
       expect(() => fn(null)).to.throw()
       expect(() => fn("77.4")).to.throw()
       expect(() => fn(true)).to.throw()
@@ -112,7 +120,7 @@ describe("Codegen tests", () => {
       expect(actual.source).to.equal("return this.abs($)")
       const fn = createFunction(actual.source)
       const numberActual = fn(-440.33)
-      expect(numberActual).to.equal(440.33)
+      expect(numberActual).to.deep.equal([440.33])
       expect(() => fn(null)).to.throw()
       expect(() => fn("1977")).to.throw()
       expect(() => fn(true)).to.throw()
@@ -167,7 +175,7 @@ describe("Codegen tests", () => {
         expect(actual.source).to.equal(`return this.datetime($)`)
         const fn = createFunction(actual.source)
         const actualDate = fn("2020-01-01")
-        expect(actualDate.getTime()).to.equal(new Date("2020-01-01").getTime())
+        expect(actualDate[0].getTime()).to.equal(new Date("2020-01-01").getTime())
       })
 
       it("template string date", () => {
@@ -175,7 +183,7 @@ describe("Codegen tests", () => {
         expect(actual.source).to.equal(`return this.datetime($,\"M/d/yyyy\")`)
         const fn = createFunction(actual.source)
         const actualDate = fn("2/21/1900")
-        expect(actualDate.getTime()).to.equal(new Date("1900-02-21").getTime())
+        expect(actualDate[0].getTime()).to.deep.equal(new Date("1900-02-21").getTime())
       })
 
       it("template string datetime with timezone", () => {
@@ -183,7 +191,7 @@ describe("Codegen tests", () => {
         expect(actual.source).to.equal(`return this.datetime($,\"M•d•yyyy@h#m#sZ\")`)
         const fn = createFunction(actual.source)
         const actualDate = fn("2•21•1900@3#35#19+8")
-        expect(actualDate.getTime()).to.equal(new Date("1900-02-21 3:35:19+8").getTime())
+        expect(actualDate[0].getTime()).to.equal(new Date("1900-02-21 3:35:19+8").getTime())
       })
     })
   })
@@ -198,19 +206,19 @@ describe("Codegen tests", () => {
         expect(objectValue).to.deep.equal([1, {c: "2"}])
         const arrayValue = fn([{"a": 1}, 77, {"b": {c: "2"}}, true, "cats"])
         expect(arrayValue).to.deep.equal([1, {c: "2"}])
-        expect(fn(null)).to.be.undefined
-        expect(fn({})).to.be.undefined
-        expect(fn([])).to.be.undefined
-        expect(fn("dogs")).to.be.undefined
-        expect(fn(707)).to.be.undefined
-        expect(fn(false)).to.be.undefined
+        expect(fn(null)).to.be.empty
+        expect(fn({})).to.be.empty
+        expect(fn([])).to.be.empty
+        expect(fn("dogs")).to.be.empty
+        expect(fn(707)).to.be.empty
+        expect(fn(false)).to.be.empty
       })
 
       it("strict .*", () => {
         const actual = generateFunctionSource("strict $.*")
         expect(actual.source).to.equal("return this.dotStar($,false)")
         const fn = createFunction(actual.source)
-        const objectValue = fn({"a": 1, "b": {c: "2"}})
+        const objectValue = fn(iterate([{"a": 1, "b": {c: "2"}}]))
         expect(objectValue).to.deep.equal([1, {c: "2"}])
         expect(() => fn([{"a": 1}, 77, {"b": {c: "2"}}, true, "cats"])).to.throw
         expect(() => fn(null)).to.throw
